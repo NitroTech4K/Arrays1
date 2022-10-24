92065dce933c5766665d09f1f46312bc80b4068a
NitroTech 4K <crtraer@gmail.com>
2022-10-24 11:56:18 -0300

    Intro to arrays

--- Program.cs
+++ Program.cs
@@ -10,6 +10,63 @@ namespace Arrays1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("==  Arrays. ==");
+            Console.WriteLine();
+
+
+            //0 Pistol
+
+            //1 Shotgun
+
+            //2 Spreader
+
+            //3 Laser
+
+            //4 Rocket Launcher
+
+
+            //current weapon
+            //string weapon = "Pistol";
+            int weapon = 0; // o..4
+
+
+            //ammo
+            int Pistolammo = 6;
+            int shotGunAmmo = 2;
+            int spreaderAmmo = 50;
+            int lazerAmmo = 25;
+            int rocketLauncherAmmo = 10;
+            //etc...
+
+            int[] ammo;
+            ammo = new int[5];
+            ammo[0] = 6;
+            ammo[1] = 2;
+            ammo[2] = 50;
+            ammo[3] = 25;
+            ammo[4] = 10;
+
+
+            //Show current weapon (WITHOUT HARD CODING!!!)
+
+            Console.WriteLine(ammo[weapon]);
+
+
+            weapon = 3; //Changing weapon
+
+
+            Console.WriteLine(ammo[weapon]);
+
+
+
+
+            Console.ReadKey(true);
+        }
+
+
+        static void Fire()
+        {
+
         }
     }
 }
